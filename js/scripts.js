a986bf7f2d503a0b31cd89a48b4cdc27ef00cb0e
Alexander Neumann and Dillon Titcomb <alexander@>
2018-01-08 10:11:42 -0800

    Added add, subtract, multiply, divide, promptFunction, promptFunction2. Able to correctly use prompts, alerts for simple calculations with support for floats.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
+var add = function(number1, number2){
+  return number1 + number2;
+}
+var subtract = function(number1, number2){
+  return number1 - number2;
+}
+var multiply = function(number1, number2){
+  return number1 * number2;
+}
+var divide = function(number1, number2){
+  return number1 / number2;
+}
+var promptFunction = function(){
+  var returnThisOne = prompt("Enter a number");
+  return returnThisOne;
+}
+var promptFunction2 = function(){
+  var returnThisToo= prompt("Enter another number");
+  return returnThisToo;
+}
+
+
 $(function(){
   $("h1").click(function() {
-    alert("Hi");
+    var number1 = promptFunction();
+    var number2 = promptFunction2();
+    alert("Your two numbers add = " + add(parseFloat(number1), parseFloat(number2)));
+    alert("Your two numbers add = " + subtract(parseFloat(number1), parseFloat(number2)));
+    alert("Your two numbers add = " + multiply(parseFloat(number1), parseFloat(number2)));
+    alert("Your two numbers add = " + divide(parseFloat(number1), parseFloat(number2)));
   });
 });
